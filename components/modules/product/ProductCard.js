d8af2f4f842fd553187b496a86731f74310c3ef3
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2023-04-16 22:00:43 -0400

    store restyle

--- components/modules/product/ProductCard.js
+++ components/modules/product/ProductCard.js
@@ -8,8 +8,8 @@ export const ProductCard = ({ product }) => {
     const price = product.node.priceRange.minVariantPrice.amount
     return (
         <Link href={`/store/${handle}`}>
-            <a className="group">
-                <div className='w-full bg-gray-200 rounded-3xl overflow-hidden'>
+            <a className="group border border-black p-2">
+                <div className='w-full bg-white'>
                     <div className='relative group-hover:opacity-75 h-72'>
                         <Image
                             src={url}
@@ -20,8 +20,8 @@ export const ProductCard = ({ product }) => {
                         />
                     </div>
                 </div>
-                <h3 className='mt-4 text-lg font-medium text-gray-900'>{title}</h3>
-                <p className='mt-1 text-sm text-gray-700'>{formatter.format(price)}</p>
+                <h3 className='mt-4 text-lg font-medium font-chivo-mono text-gray-900'>{title}</h3>
+                <p className='mt-1 text-sm font-chivo-mono text-gray-700'>{formatter.format(price)}</p>
             </a>
         </Link>
     )
