0399bb71d0d0964f565dba65bded02f437d63e84
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2023-04-16 22:26:43 -0400

    fixing width going off screen

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -3,7 +3,7 @@ import Nav from '../controls/Nav'
 import Footer from '../controls/Footer'
 export default function MainLayout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen min-w-full bg-white items-center mx-1 md:max-w-[1666px] justify-between'>
+    <div className='flex flex-col min-h-screen min-w-[97%] bg-white items-center mx-1 md:max-w-[1666px] justify-between'>
       <Nav />
       <main className='mt-10 w-full'>
         <div>
