4d34bb45e64744e87d20082571449a17ceae1c7b
Frank Costoya <fcostoyaprograms@gmail.com>
2025-12-03 21:13:47 -0500

    Update Title

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,15 @@ import '../styles/fonts.css';
 import ShopProvider from '../context/shopContext'
 import { useRouter } from 'next/router'
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   return (
     <>
+    <Head>
+      <title>DSJD</title>
+    </Head>
       <ShopProvider>
         <Component {...pageProps} key={router.asPath} />
       </ShopProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* <link rel="alternate icon" href="/favicon.ico" /> */}
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> */}
       </Head>
       <body>
         <Main />
